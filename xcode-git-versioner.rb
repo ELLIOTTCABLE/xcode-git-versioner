f5226f5d3321d1b76e14083462bab26c23ad73a1
elliottcable <git@elliottcable.name>
2008-12-18 17:13:31 -0900

    Forgot to actually use the proper git location as discovered by `which`. Oops! heh <,<

--- xcode-git-versioner.rb
+++ xcode-git-versioner.rb
@@ -7,7 +7,7 @@ plist = File.join ENV['BUILT_PRODUCTS_DIR'], ENV['INFOPLIST_PATH']
 raise "Info.plist missing, or path error" unless File.file? plist
 
 git = `sh /etc/profile; which git`.chomp
-tag, age, sha = `git describe --tags --always --long`.chomp.match(/(?:(.*)-(\d+)-)?([0-9a-g]+)/i)[1..3]
+tag, age, sha = `#{git} describe --tags --always --long`.chomp.match(/(?:(.*)-(\d+)-)?([0-9a-g]+)/i)[1..3]
 
 # "v1.0", "v1.0b2 [g3f22c9f]", "[g3f22c9f]"
 version = tag ? (age.to_i.zero? ? "#{tag}" : "#{tag}b#{age} [#{sha}]") : "[#{sha}]"
